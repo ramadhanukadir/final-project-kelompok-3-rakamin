c613ab156a79f549aec0021dfd3ccf17ccddb293
misbahabroruddin <misbahabroruddin@gmail.com>
2023-06-22 19:40:02 +0700

    setup express

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
