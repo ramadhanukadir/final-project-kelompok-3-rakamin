ce0cf9a23288cbbc783c1281cfa4da52dac2b528
misbahabroruddin <misbahabroruddin@gmail.com>
2023-06-23 14:05:15 +0700

    add seeding

---
+++ server/seeders/20230623065151-Users.js
@@ -0,0 +1,39 @@
+"use strict";
+const bcrypt = require("bcrypt");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("Users", [
+      {
+        first_name: "Admin",
+        last_name: "Admin",
+        username: "admin",
+        email: "admin@gmail.com",
+        password: await bcrypt.hash("admin", 10),
+        image_url:
+          "https://upload.wikimedia.org/wikipedia/commons/b/b5/Windows_10_Default_Profile_Picture.svg",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
