0b0d6a242ea0e0f16ec4b4f605d559c5344d79da
misbahabroruddin <misbahabroruddin@gmail.com>
2023-06-28 22:26:27 +0700

    fix index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,33 @@
-const app = require("./app");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
 const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT;
+const ItemsRoute = require("./routes/items");
+const CategoriesRoute = require("./routes/categories");
+const OrdersRoute = require("./routes/orders");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  morgan("combined", {
+    skip: function (req, res) {
+      return res.statusCode < 400;
+    },
+  })
+);
+app.use(ItemsRoute);
+app.use(CategoriesRoute);
+app.use(OrdersRoute);
+
+app.get("/ping", (req, res) => {
+  try {
+    res.json({ ping: "success" });
+  } catch (error) {
+    console.log("something went wrong");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
