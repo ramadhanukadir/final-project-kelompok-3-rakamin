880d5c81d20d319851348a397217754dda102f50
misbahabroruddin <misbahabroruddin@gmail.com>
2023-06-30 17:23:43 +0700

    add middleware upload photo

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const upload = require("./../middlewares/uploadPhoto");
 const router = express.Router();
 const ItemsController = require("../controllers/ItemsController");
 
@@ -6,9 +7,13 @@ router.get("/items", ItemsController.getAllItems);
 
 router.get("/items/:id", ItemsController.getItemsById);
 
-router.post("/items", ItemsController.createItems);
+router.post("/items", upload.single("image_url"), ItemsController.createItems);
 
-router.put("/items/:id", ItemsController.updateItems);
+router.put(
+  "/items/:id",
+  upload.single("image_url"),
+  ItemsController.updateItems
+);
 
 router.delete("/items/:id", ItemsController.deleteItems);
 
