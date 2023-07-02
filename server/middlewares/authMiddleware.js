38cc4e5616ac9e3db1e6c1871d37bd7fb2721a58
iqbalarief <iqbal98arief@gmail.com>
2023-07-02 16:38:32 +0700

    Adding Logged Users

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,12 +1,33 @@
 const jwt = require("jsonwebtoken");
-const authMiddleware = (req, res, next) => {
-
+const { Users } = require("../models");
+const authMiddleware = async (req, res, next) => {
+  try {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
-    if (token == null) return res.sendStatus(401);
-    
+    if (token == null) {
+      return res.sendStatus(401);
+    }
+
     const user = jwt.verify(token, process.env.JWT_SECRETKEY);
-    req.userId = user.userId;
+    const foundUsers = await Users.findOne({
+      where: { id: user.userId },
+    });
+
+    if (!foundUsers) {
+      throw { name: "error not found" };
+    }
+
+    req.loggedUser = {
+      id: foundUsers.id,
+      first_name: foundUsers.first_name,
+      last_name: foundUsers.last_name,
+      username: foundUsers.username,
+      email: foundUsers.email,
+    };
+
     next();
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 module.exports = authMiddleware;
