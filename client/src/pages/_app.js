f4175109963b722fb95279427ab50a867a74e077
misbahabroruddin <misbahabroruddin@gmail.com>
2023-07-05 14:20:08 +0700

    fix styling

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,24 +1,24 @@
-import { ChakraProvider, Grid, Box } from "@chakra-ui/react";
-import "@/styles/globals.css";
-import { Navbar } from "@/component/landingComponent";
-import SideBar from "@/component/landingComponent/SideBar";
-import { useRouter } from "next/router";
+import { ChakraProvider, Grid, Box } from '@chakra-ui/react';
+import '@/styles/globals.css';
+import { Navbar } from '@/component/landingComponent';
+import SideBar from '@/component/landingComponent/SideBar';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   const isRootPage =
-    router.pathname === "/" ||
-    router.pathname === "/register" ||
-    router.pathname === "/login";
+    router.pathname === '/' ||
+    router.pathname === '/register' ||
+    router.pathname === '/login';
   return (
     <ChakraProvider>
-      <Grid height="" templateRows="auto 1fr" templateColumns="1fr" gap={0}>
+      <Grid height='' templateRows='auto 1fr' templateColumns='1fr' gap={0}>
         {!isRootPage && (
-          <Box templateColumns="auto 1fr" height="">
+          <Box templateColumns='auto 1fr' height=''>
             <SideBar />
           </Box>
         )}
-        <Box p={5}>
+        <Box height={'100vh'}>
           <Component {...pageProps} />
         </Box>
       </Grid>
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   Container,
   Stack,
@@ -14,116 +14,127 @@ import {
   InputGroup,
   InputRightElement,
   Link,
-} from "@chakra-ui/react";
-import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
-import Image from "next/image";
-import Image2 from "../image/cloud-01.png";
-import { useRouter } from "next/router";
+} from '@chakra-ui/react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import Image from 'next/image';
+import Image2 from '../image/cloud-01.png';
+import { useRouter } from 'next/router';
 
 const login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const router = useRouter();
 
   const handleHome = () => {
-    router.push("/");
+    router.push('/');
   };
 
   const handleLoginClick = () => {
-    router.push("/dashboard");
+    router.push('/dashboard');
   };
   return (
-    <Container maxW={"7xl"}>
+    <Container maxW={'7xl'} height={'inherit'} py={{ base: 10, md: 8 }}>
       <Stack
-        align={"center"}
-        spacing={{ base: 8, md: 10 }}
-        py={{ base: 20, md: 28 }}
-        direction={{ base: "column", md: "row" }}>
-        <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+        align={'center'}
+        maxH={'full'}
+        // spacing={{ base: 8, md: 10 }}
+        // py={{ base: 20, md: 28 }}
+        direction={{ base: 'column', md: 'row' }}
+      >
+        <Stack flex={1} height={'inherit'}>
           <Image
-            src="/StocktrackrLogo-01.png"
-            alt="logo"
+            src='/StocktrackrLogo-01.png'
+            alt='logo'
             width={150}
             height={150}
             onClick={handleHome}
           />
           <Heading
             lineHeight={1.1}
-            fontWeight="bold"
-            fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}>
+            fontWeight='bold'
+            fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}
+          >
             <Text
-              as={"span"}
-              position={"relative"}
+              as={'span'}
+              position={'relative'}
               _after={{
                 content: "''",
-                position: "absolute",
+                position: 'absolute',
                 bottom: 1,
-                bg: "blue.400",
+                bg: 'blue.400',
                 zIndex: -1,
-              }}>
+              }}
+            >
               Welcome Back !
             </Text>
-            <Text fontSize="2xl">Log in to Your Account</Text>
+            <Text fontSize='2xl'>Log in to Your Account</Text>
           </Heading>
           <FormControl>
             <FormLabel>Email address</FormLabel>
-            <Input type="email" />
+            <Input type='email' />
             <FormHelperText>We'll never share your email.</FormHelperText>
             <FormLabel>Password</FormLabel>
             <InputGroup>
-              <Input type={showPassword ? "text" : "password"} />
-              <InputRightElement h={"full"}>
+              <Input type={showPassword ? 'text' : 'password'} />
+              <InputRightElement h={'full'}>
                 <Button
-                  variant={"ghost"}
+                  variant={'ghost'}
                   onClick={() =>
                     setShowPassword((showPassword) => !showPassword)
-                  }>
+                  }
+                >
                   {showPassword ? <ViewIcon /> : <ViewOffIcon />}
                 </Button>
               </InputRightElement>
             </InputGroup>
             <Text>
-              New here?{" "}
-              <Link href="/register" fontWeight="bold">
-                {" "}
+              New here?{' '}
+              <Link href='/register' fontWeight='bold'>
+                {' '}
                 Create an Account!
               </Link>
             </Text>
           </FormControl>
           <Stack
             spacing={{ base: 4, sm: 6 }}
-            direction={{ base: "column", sm: "row" }}>
+            direction={{ base: 'column', sm: 'row' }}
+          >
             <Button
-              rounded={"lg"}
-              size={"lg"}
-              fontWeight={"Bold"}
+              rounded={'lg'}
+              size={'lg'}
+              fontWeight={'Bold'}
               px={20}
-              colorScheme={"red"}
-              bg={"blue.400"}
-              _hover={{ bg: "blue.500" }}
-              onClick={handleLoginClick}>
+              colorScheme={'red'}
+              bg={'blue.400'}
+              _hover={{ bg: 'blue.500' }}
+              onClick={handleLoginClick}
+            >
               Login
             </Button>
           </Stack>
         </Stack>
         <Flex
           flex={1}
-          justify={"center"}
-          align={"center"}
-          position={"relative"}
-          w={"full"}>
+          justify={'center'}
+          align={'center'}
+          // position={'relative'}
+          width={'full'}
+          height={'auto'}
+          // p={5}
+        >
           <Box
-            position={"relative"}
-            height={{ base: "300px", sm: "300px", lg: "500px" }}
-            rounded={"2xl"}
-            width={"full"}
-            overflow={"hidden"}>
+            // position={'relative'}
+            // height={{ base: '300px', sm: '300px', lg: '500px' }}
+            // rounded={'2xl'}
+            width={'full'}
+            // overflow={'hidden'}
+          >
             <Image
               src={Image2}
-              alt={"Hero Image"}
-              fit={"cover"}
-              align={"center"}
-              w={"100%"}
-              h={"100%"}
+              alt={'Hero Image'}
+              fit={'cover'}
+              // align={'center'}
+              w={'100%'}
+              h={'auto'}
             />
           </Box>
         </Flex>
