20bda898d157244d21d38d303cbc651193b5d267
misbahabroruddin <misbahabroruddin@gmail.com>
2023-07-06 13:57:40 +0700

    create context

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -3,6 +3,7 @@ import '@/styles/globals.css';
 import { Navbar } from '@/component/landingComponent';
 import SideBar from '@/component/landingComponent/SideBar';
 import { useRouter } from 'next/router';
+import AllDataContextProvider from '@/context/AllDataContext';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
@@ -19,7 +20,9 @@ export default function App({ Component, pageProps }) {
           </Box>
         )}
         <Box height={'100vh'}>
-          <Component {...pageProps} />
+          <AllDataContextProvider>
+            <Component {...pageProps} />
+          </AllDataContextProvider>
         </Box>
       </Grid>
     </ChakraProvider>
