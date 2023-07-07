f24de3ac08c0c3dd832fb71ed193554ee0b20fa8
misbahabroruddin <misbahabroruddin@gmail.com>
2023-07-07 13:35:48 +0700

    fix path

--- server/routes/suppliersItems.js
+++ server/routes/suppliersItems.js
@@ -1,10 +1,10 @@
-const router = require("express").Router();
-const controller = require("../controllers/Supplier-Items.Controller");
+const router = require('express').Router();
+const controller = require('./../controllers/SupplierItemsController');
 
-router.post("/", controller.postSupplierItems);
-router.put("/:id", controller.updateSupplierItems);
-router.get("/", controller.getAllSupplierItems);
-router.get("/:id", controller.getIdSuppliersItems);
-router.delete("/:id", controller.deleteSuppliersItems);
+router.post('/', controller.postSupplierItems);
+router.put('/:id', controller.updateSupplierItems);
+router.get('/', controller.getAllSupplierItems);
+router.get('/:id', controller.getIdSuppliersItems);
+router.delete('/:id', controller.deleteSuppliersItems);
 
 module.exports = router;
