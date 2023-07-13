87c310c4815820e02e649dccdbb3f5fa6ce31cab
misbahabroruddin <misbahabroruddin@gmail.com>
2023-07-13 23:45:49 +0700

    fix order pages

--- client/src/pages/purchase/index.js
+++ client/src/pages/purchase/index.js
@@ -1,27 +1,28 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Box, Button, Container, Flex, VStack } from "@chakra-ui/react";
-import { DataContext } from "@/context/AllDataContext";
-import OrderForm from "@/component/OrderForm/OrderForm";
-import { useRouter } from "next/router";
-import OrderList from "@/component/OrderList/OrderList";
-import { ArrowBackIcon } from "@chakra-ui/icons";
+import React, { useContext, useEffect, useState } from 'react';
+import { Box, Button, Container, Flex, VStack } from '@chakra-ui/react';
+import { DataContext } from '@/context/AllDataContext';
+import OrderForm from '@/component/OrderForm/OrderForm';
+import { useRouter } from 'next/router';
+import OrderList from '@/component/OrderList/OrderList';
+import { ArrowBackIcon } from '@chakra-ui/icons';
 
 const index = () => {
   const { orders } = useContext(DataContext);
 
   return (
-    <VStack pt={16} zIndex={1}>
+    <VStack pt={20} zIndex={1}>
       <Flex
-        w={"100%"}
-        justifyContent={"space-between"}
-        alignSelf={"flex-end"}
-        zIndex={1}>
-        <Button size={"sm"}>
+        w={'100%'}
+        justifyContent={'space-between'}
+        alignSelf={'flex-end'}
+        zIndex={1}
+      >
+        <Button size={'sm'} zIndex={1}>
           <ArrowBackIcon w={4} h={4} />
         </Button>
         <OrderForm />
       </Flex>
-      <Box w={"100%"}>
+      <Box w={'100%'} mt={8}>
         <OrderList orders={orders} />
       </Box>
     </VStack>
