42e229d63eb90f189b282b9f2f2714762b4e8e85
misbahabroruddin <misbahabroruddin@gmail.com>
2023-07-20 20:10:45 +0700

    fix styling dashboard

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,42 +1,8 @@
 import React, { useContext, useState } from 'react';
-import {
-  Flex,
-  Heading,
-  Avatar,
-  AvatarGroup,
-  Text,
-  Icon,
-  IconButton,
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
-  Divider,
-  Link,
-  Box,
-  Button,
-  Input,
-  InputGroup,
-  InputLeftElement,
-} from '@chakra-ui/react';
-import {
-  FiHome,
-  FiPieChart,
-  FiDollarSign,
-  FiBox,
-  FiCalendar,
-  FiChevronDown,
-  FiChevronUp,
-  FiPlus,
-  FiCreditCard,
-  FiBell,
-} from 'react-icons/fi';
+import { Flex, Heading } from '@chakra-ui/react';
 import MyChart from '@/component/dataComponent/chart/LineChart';
 import ExpensesChart from '@/component/dataComponent/chart/expenses';
 import { DataContext } from '@/context/AllDataContext';
-import { formatter } from '@/modules/formatter';
 
 export default function Dashboard() {
   const [display, changeDisplay] = useState('hide');
@@ -63,119 +29,17 @@ export default function Dashboard() {
         w={'100%'}
         columnGap={4}
       >
-        <Flex
-          // w={['100%', '100%', '60%', '60%', '55%']}
-          // border={'1px solid #DFF6FF'}
-          // borderRadius={'10px'}
-          p='3%'
-          flexDir='column'
-          overflow='auto'
-          // minH='100vh'
-        >
+        <Flex p='3%' flexDir='column' overflow='auto'>
           <Heading fontWeight='normal' mb={4} letterSpacing='tight'>
             Revenue History
           </Heading>
-
           <MyChart />
-          {/* <Flex justifyContent='space-between' mt={8}>
-          <Flex align='flex-end'>
-            <Heading as='h2' size='lg' letterSpacing='tight'>
-              Transactions
-            </Heading>
-            <Text fontSize='small' color='gray' ml={4}>
-              Apr 2021
-            </Text>
-          </Flex>
-        </Flex> */}
-          {/* <Flex flexDir='column'>
-          <Flex overflow='auto'>
-            <Table variant='unstyled' mt={4}>
-              <Thead>
-                <Tr color='gray'>
-                  <Th>Name of transaction</Th>
-                  <Th>Quantity</Th>
-                  <Th isNumeric>Price</Th>
-                  <Th isNumeric>Total Price</Th>
-                </Tr>
-              </Thead>
-
-              <Tbody>
-                {orderData.map((order) => (
-                  <>
-                    {display == 'show' && (
-                      <>
-                        <Tr key={order.id}>
-                          <Td>
-                            <Flex align='center'>
-                              <Avatar
-                                size='sm'
-                                mr={2}
-                                src={order.Item.image_url}
-                              />
-                              <Flex flexDir='column'>
-                                <Heading size='sm' letterSpacing='tight'>
-                                  {order.Item.name}
-                                </Heading>
-                                <Text fontSize='sm' color='gray'>
-                                  {new Date(order.createdAt).toLocaleString(
-                                    'en-US',
-                                    { timeZone: 'Asia/Jakarta' }
-                                  )}
-                                </Text>
-                              </Flex>
-                            </Flex>
-                          </Td>
-                          <Td>{order.quantity}</Td>
-                          <Td isNumeric>
-                            <Text fontWeight='bold' display='inline-table'>
-                              IDR
-                            </Text>
-                            {formatter.format(order.Item.selling_price)}
-                          </Td>
-                          <Td isNumeric>
-                            <Text fontWeight='bold' display='inline-table'>
-                              IDR
-                            </Text>
-                            {formatter.format(order.total_price)}
-                          </Td>
-                        </Tr>
-                      </>
-                    )}
-                  </>
-                ))}
-              </Tbody>
-            </Table>
-          </Flex>
-          <Flex align='center'>
-            <Divider />
-            <IconButton
-              icon={display == 'show' ? <FiChevronUp /> : <FiChevronDown />}
-              onClick={() => {
-                if (display == 'show') {
-                  changeDisplay('none');
-                } else {
-                  changeDisplay('show');
-                }
-              }}
-            />
-            <Divider />
-          </Flex>
-        </Flex> */}
         </Flex>
-
-        {/* Column 3 */}
         <Flex
-          // w={['100%', '100%', '40%']}
-          // border={'1px solid #DFF6FF'}
-          // borderRadius={'10px'}
           p='2%'
           flexDir='column'
-          // overflow="auto"
           minW={[null, null, '100px', '100px', '100px']}
         >
-          {/* <Heading letterSpacing='tight' textAlign={'center'}>
-            Profit
-          </Heading> */}
           <ExpensesChart />
         </Flex>
       </Flex>
