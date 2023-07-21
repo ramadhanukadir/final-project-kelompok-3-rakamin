56e06655ce927b43804692615697f3aa0087254a
misbahabroruddin <misbahabroruddin@gmail.com>
2023-07-21 14:11:22 +0700

    add endpoint

--- client/src/api/suppliers/index.js
+++ client/src/api/suppliers/index.js
@@ -1,8 +1,12 @@
-import { instance } from "@/modules/axios";
+import { instance } from '@/modules/axios';
 
-export const getAllSuppliers = async () => {
+export const getAllSuppliers = async (filters = {}) => {
   try {
-    const { data } = await instance.get("/suppliers");
+    const nonEmptyFilters = Object.fromEntries(
+      Object.entries(filters).filter(([_, value]) => value !== '')
+    );
+    const params = new URLSearchParams(nonEmptyFilters).toString();
+    const { data } = await instance.get(`/suppliers?${params}`);
     return data;
   } catch (error) {
     throw new Error(error.response.data.message);
@@ -14,6 +18,6 @@ export const getSuppliersById = async (id) => {
     const { data } = await instance.get(`/suppliers/${id}`);
     return data.dataSuppliers;
   } catch (error) {
-    console.error("Gagal mengambil data:", error);
+    console.error('Gagal mengambil data:', error);
   }
 };
